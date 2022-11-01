c1d4339e457be73f0ff9fe815ff6682ac2ed05e9
EverVino <EverVino@users.noreply.github.com>
2022-11-01 01:14:53 +0000

    deploy: 01f1b59f2c9ca733f4ebf631bedf2ddb5d0c7df3

--- pt/js/bundle.26eb2e8106f857a4b6d2469463b389954f1f2da018ef77840a159131fbef474520b6b0a47d47a0f3067ebeb79ce19ae3f814bf9bd678000dbc86d6d3f6ed3064.js
+++ pt/js/bundle.26eb2e8106f857a4b6d2469463b389954f1f2da018ef77840a159131fbef474520b6b0a47d47a0f3067ebeb79ce19ae3f814bf9bd678000dbc86d6d3f6ed3064.js
@@ -8,7 +8,7 @@ const scaleImageClass = 'image-scale';
 const pageHasLoaded = 'DOMContentLoaded';
 const imageAltClass = 'img_alt'
 
-const baseURL = 'https://osl-hugo.github.io';
+const baseURL = 'https://osl-incubator.github.io/osl-hugo.github.io/';
 const searchFieldClass = '.search_field';
 const searchClass = '.search';
 const goBackClass = 'button_back';
