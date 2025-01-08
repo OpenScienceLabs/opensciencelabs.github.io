b2b4745f6aeb227bbcd5417a492ac1321a1cf155
xmnlab <xmnlab@users.noreply.github.com>
2025-01-08 04:33:28 +0000

    deploy: 5e5d4504e5e62f84d457fb0160752cf222ed9bc2

--- blog/typer-a-python-library-for-building-cli-applications/ecommerce.py
+++ blog/typer-a-python-library-for-building-cli-applications/ecommerce.py
@@ -1,7 +1,8 @@
+"""E-commerce module."""
+
 import typer
 
 from typer import Context, Option
-
 
 app = typer.Typer(help="Operations for e-commerce.")
 app_user = typer.Typer(help="Operations for user model.")
@@ -9,6 +10,7 @@ app_product = typer.Typer(help="Operations for product model.")
 
 app.add_typer(app_user, name="user")
 app.add_typer(app_product, name="product")
+
 
 @app.callback(invoke_without_command=True)
 def main(
@@ -31,25 +33,35 @@ def main(
         typer.echo(ctx.get_help())
         raise typer.Exit(0)
 
+
 @app_user.command("create")
-def user_create(name: str = typer.Argument(..., help="Name of the user to create.")) -> None:
+def user_create(
+    name: str = typer.Argument(..., help="Name of the user to create."),
+) -> None:
     """Create a new user with the given name."""
     print(f"Creating user: {name} - Done")
 
 
 @app_user.command("update")
-def user_update(name: str = typer.Argument(..., help="Name of the user to update.")) -> None:
+def user_update(
+    name: str = typer.Argument(..., help="Name of the user to update."),
+) -> None:
     """Update user data with the given name."""
     print(f"Updating user: {name} - Done")
 
+
 @app_product.command("create")
-def product_create(name: str = typer.Argument(..., help="Name of the product to create.")) -> None:
+def product_create(
+    name: str = typer.Argument(..., help="Name of the product to create."),
+) -> None:
     """Create a new product with the given name."""
     print(f"Creating product: {name} - Done")
 
 
 @app_product.command("update")
-def product_update(name: str = typer.Argument(..., help="Name of the product to update.")) -> None:
+def product_update(
+    name: str = typer.Argument(..., help="Name of the product to update."),
+) -> None:
     """Update a product with the given name."""
     print(f"Updating product: {name} - Done")
 
--- blog/typer-a-python-library-for-building-cli-applications/greet.py
+++ blog/typer-a-python-library-for-building-cli-applications/greet.py
@@ -1,18 +1,19 @@
+"""Greeting module."""
 
 import typer
 
 app = typer.Typer()
 
+
 @app.command()
 def greet(
     name: str = typer.Option(
-        ..., 
-        "--name",
-        help="The name of the person to greet."
-    )
+        ..., "--name", help="The name of the person to greet."
+    ),
 ) -> None:
     """Greets the user by name."""
     typer.echo(f"Hello {name}!")
+
 
 if __name__ == "__main__":
     app()
